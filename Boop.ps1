dead485168ebfe7ad595f16b6a182c8221020cbc
Ting3l <henning@gehebajo.de>
2020-08-21 19:14:05 +0200

    Update Boop.ps1 (v1.2)
    Made $ip non-mandatory to show help when no param is given.
    Added "-help" parameter
    Replacing now a function
    Made the receive-job-loop from while to do-while & added a sleep after ending to compensate slow output
    Added changelog

--- Boop.ps1
+++ Boop.ps1
@@ -1,6 +1,14 @@
-﻿function boop{
+function replace-ping($in){
+    return $in -creplace "ping","boop" `
+               -creplace "Ping","Boop" `
+               -creplace "-R ","-RH" `
+               -creplace "-R]","-RH]" `
+               -creplace "-S srcaddr  ","-src srcaddr" `
+               -creplace "-S srcaddr]","-src srcaddr]" 
+}
+function boop{
     param(
-        [parameter(Mandatory=$true,Position=0)]
+        [parameter(Position=0)]
         [String]$ip,
 
         [parameter()]
@@ -52,7 +60,10 @@
         [switch]$4,
 
         [parameter()]
-        [switch]$6
+        [switch]$6,
+
+        [parameter()]
+        [switch]$help
     )
     try{
         $paramStr = ""
@@ -63,6 +74,7 @@
         if($p){$paramStr+="-p "}
         if($4){$paramStr+="-4 "}
         if($6){$paramStr+="-6 "}
+        if($help -or ($ip -eq "")){$paramStr+="-? "}
         
         if($r -ne -1){$paramStr+="-r $r "}
         if($s -ne -1){$paramStr+="-s $s "}
@@ -75,16 +87,11 @@
 
         Start-Job -Name Boop -ScriptBlock {ping $args[0] $args[1] -n $args[2] -l $args[3] -i $args[4] -v $args[5]} -ArgumentList $ip,$paramStr,$n,$l,$i,$v | Out-Null
     
-        while((Get-Job -Name Boop).State -eq "Running"){
-            (Receive-Job -Name Boop) | foreach{
-                $_ -creplace "ping","boop" `
-                   -creplace "Ping","Boop" `
-                   -creplace "-R ","-RH" `
-                   -creplace "-R]","-RH]" `
-                   -creplace "-S srcaddr  ","-src srcaddr" `
-                   -creplace "-S srcaddr]","-src srcaddr]" 
-            }
-        }
+        do{
+            (Receive-Job -Name Boop) | foreach{replace-ping $_}
+        }while((Get-Job -Name Boop).State -eq "Running")
+        Start-Sleep -Milliseconds 100
+        (Receive-Job -Name Boop) | foreach{replace-ping $_}
     }
     catch{}
     finally{Remove-Job -Name Boop -Force | Out-Null}
@@ -94,4 +101,16 @@
 # https://twitter.com/jessica_schalz/status/1292973216634372097
 
 # Written by @Ting3l
-# V 1.1
+# V 1.2
+
+
+# Changelog
+###############
+#
+# Ting3l    V1.0    Initial
+# Ting3l    V1.1    case-sensitive replacing to correct output "booboop" to "booping"
+# Ting3l    V1.2    Made $ip non-mandatory to show help when no param is given.
+#                   Added "-help" parameter
+#                   Replacing now a function
+#                   Made the receive-job-loop from while to do-while & added a sleep after ending to compensate slow output
+#                   Added changelog
